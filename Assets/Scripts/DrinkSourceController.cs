930f69bf1b9c9bd2c2f63435b79092e4e9d963a9
Aidan <aidanoflann@gmail.com>
2017-06-17 19:24:51 +0100

    missed new cs's

---
+++ Assets/Scripts/DrinkSourceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class DrinkSourceController
+    {
+        private Item _storedItem;
+
+        public DrinkSourceController(ItemType type, int count)
+        {
+            this._storedItem = new Item(type, count);
+        }
+
+        public ItemType GetItem()
+        {
+            // TODO: can handle cooldown and duration here
+            this._storedItem.Count--;
+            return this._storedItem.type;
+        }
+    }
+}
