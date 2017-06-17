911fda92b7cf07e48ed28cb25a53cacf1a000e04
Steebi <farrelss@tcd.ie>
2017-06-17 16:49:27 +0100

    Forgot to add shit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = -(Input.GetAxis("Vertical"));
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+
+    }
+}
