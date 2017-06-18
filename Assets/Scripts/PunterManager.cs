1c7acfef80182e62b6a4b596dedba9889f214ab7
Aidan <aidanoflann@gmail.com>
2017-06-18 11:48:06 +0100

    fleshed out PunterManager

--- Assets/Scripts/PunterManager.cs
+++ Assets/Scripts/PunterManager.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     class PunterManager
     {
+        public GameObject punterToClone;
+
+        public int MaxPunters;
+        public float SpawnPeriod;
+
+        private DateTime _nextSpawnTime = DateTime.Now;
+
+        private List<int> _punterIDs;
+
+        public void Tick(float deltaTime)
+        {
+            DateTime Now = DateTime.Now;
+            if (this._nextSpawnTime > Now)
+            {
+                this._punterIDs.Add(this.SpawnPunter());
+                this.UpdateNextSpawnTime(Now);
+            }
+        }
+
+        private int SpawnPunter()
+        {
+            Debug.Log("Spawning a punter!");
+            return 1;
+        }
+        
+        private void UpdateNextSpawnTime(DateTime now)
+        {
+            // TODO: add randomness
+            this._nextSpawnTime = now.AddSeconds(this.SpawnPeriod);
+        }
     }
 }
