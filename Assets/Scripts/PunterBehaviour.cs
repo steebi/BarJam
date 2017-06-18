1cef4e8174f5c29fe35c96eb15a5bff3daabf931
Steebi <farrelss@tcd.ie>
2017-06-18 14:09:15 +0100

    Unfixed the rotate collider thing. Will just disable the collider instead...

--- Assets/Scripts/PunterBehaviour.cs
+++ Assets/Scripts/PunterBehaviour.cs
@@ -7,8 +7,17 @@ public class PunterBehaviour : MonoBehaviour {
 
     public PunterController punterController;
 
+    [SerializeField]
+    private CapsuleCollider fixedColliderTransform;
+
 	// Use this for initialization
 	void Start () {
         punterController = new PunterController();
 	}
+
+    private void Update()
+    {
+        // Removed this. Will disable the collider when they are leaving instead
+        // FixedColliderTransform.transform.rotation = Quaternion.identity;
+    }
 }
