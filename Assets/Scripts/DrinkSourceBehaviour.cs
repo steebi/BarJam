14a21d408dc7ab7a0ff7942bdab3094a58379dae
Aidan <aidanoflann@gmail.com>
2017-06-18 13:04:33 +0100

    missed DrinkSourceBehaviour

---
+++ Assets/Scripts/DrinkSourceBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class DrinkSourceBehaviour : MonoBehaviour {
+
+    public DrinkSourceController DrinkSourceController;
+
+	// Use this for initialization
+	void Start () {
+        // TODO: default to Beer for now, but make configurable later
+        this.DrinkSourceController = new DrinkSourceController(ItemType.Beer, 100);
+	}
+}
