ba94a6c9eca1b9a94b85c3ab7bfc481200278ff3
Aidan <aidanoflann@gmail.com>
2017-06-25 15:55:22 +0100

    running request on thread

--- Assets/Scripts/WebLayer/RequestHandler.cs
+++ Assets/Scripts/WebLayer/RequestHandler.cs
@@ -5,29 +5,36 @@ using UnityEngine.Networking;
 public class RequestHandler
 {
     // to be used for running requests on a thread.
-    private RequestBehaviour _requestBehviour;
+    private RequestBehaviour _requestBehaviour;
     private GameObject _requestBehaviourGO;
 
     public RequestHandler()
     {
         this._requestBehaviourGO = new GameObject();
-        this._requestBehaviourGO.AddComponent<RequestBehaviour>();
+        this._requestBehaviour = this._requestBehaviourGO.AddComponent<RequestBehaviour>();
     }
 
     public void MakeRequest(object requestObject, string endPoint, string value)
+    {
+        this._requestBehaviour.MakeRequest(this.CreateRequestEnumerator(requestObject, endPoint, value));
+        
+    }
+
+    private IEnumerator CreateRequestEnumerator(object requestObject, string endPoint, string value)
     {
         string json = JsonUtility.ToJson(requestObject);
         UnityWebRequest request = UnityWebRequest.Put(endPoint + value, json);
         request.method = "POST";
         request.SetRequestHeader("Content-Type", "application/json");
-        //TODO: handle timeouts?
-        //TODO: store a reference to a MonoBehaviour which can run this on a thread - requests might be slow
-        request.Send();
+        yield return request.Send();
     }
 }
 
 class RequestBehaviour : MonoBehaviour
 {
-
+    public void MakeRequest(IEnumerator requestEnumerator)
+    {
+        StartCoroutine(requestEnumerator);
+    }
 }
 
