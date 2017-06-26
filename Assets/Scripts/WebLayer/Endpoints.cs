8743090dc7846651010d7f25a579edaee2edd691
Aidan <aidanoflann@gmail.com>
2017-06-26 19:29:12 +0100

    updated endpoint, uses 80 now

--- Assets/Scripts/WebLayer/Endpoints.cs
+++ Assets/Scripts/WebLayer/Endpoints.cs
@@ -5,5 +5,5 @@ public class Endpoints
 {
     public static readonly string GAME_NAME = "tendr";
 
-    public static readonly string SCORE_BOARD = "http://scores.aidanof.com:5000/";
+    public static readonly string SCORE_BOARD = "http://scores.aidanof.com/";
 }
