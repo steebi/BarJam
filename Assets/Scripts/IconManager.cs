f9e1b94a78551fe2bd25af116934f0d718bed6c7
Steebi <farrelss@tcd.ie>
2018-01-08 15:42:11 +0000

    forgot to add the files D:

---
+++ Assets/Scripts/IconManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts{
+
+	public class IconManager : Singleton<IconManager> {
+
+		public List<Sprite> m_itemSprites = new List<Sprite>();
+		public List<Sprite> m_numberSprites = new List<Sprite>();
+		public Sprite m_errorSprite;
+
+		public Sprite GetItemSprite(ItemType itemtype){
+			int position = (int)itemtype;
+			if(position < m_itemSprites.Count)
+				return m_itemSprites[position];
+			else
+				return m_errorSprite;
+		}	
+
+		public Sprite GetNumberSprite(int count){
+			if(count < m_numberSprites.Count)
+				return m_numberSprites[count];
+			else
+				return m_errorSprite;
+		}
+
+	}
+}
