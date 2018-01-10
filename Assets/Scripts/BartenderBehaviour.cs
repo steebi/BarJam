1e6543bee2151bc9dfccfe1dd532f839d22b2800
Aidan <aidanoflann@gmail.com>
2018-01-10 09:59:34 +0900

    Revert "Resctructed scripts to be a bit more manageable"
    This reverts commit 30e6cf073b59a67db6b1a61186efb7451a0e9867.

---
+++ Assets/Scripts/BartenderBehaviour.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class BartenderBehaviour : MonoBehaviour
+{
+
+    public int speed;
+
+    private Rigidbody rb;
+    public BartenderController BartenderController;
+    private PunterBehaviour punter = null;
+    private DrinkSourceBehaviour drinkSourceBehavour = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        BartenderController = new BartenderController();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("TalkToPunter"))
+        {
+            if (punter != null)
+            {
+                BartenderController.TalkToPunter(punter.punterController);
+            }
+            else if (this.drinkSourceBehavour != null)
+            {
+                BartenderController.AccessDrinkSource(this.drinkSourceBehavour.DrinkSourceController);
+                Debug.Log("Accessing Drink Source");
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = -(Input.GetAxis("Vertical"));
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, moveVertical);
+        movement = Quaternion.Euler(0, -45, 0) * movement;
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Punter")
+        {
+            Debug.Log("Attaching Punter.");
+            punter = other.GetComponentInParent<PunterBehaviour>();
+        }
+        else if (other.tag == "DrinkSource")
+        {
+            Debug.Log("Attaching DrinkSource.");
+            this.drinkSourceBehavour = other.GetComponent<DrinkSourceBehaviour>();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Punter")
+        {
+            Debug.Log("Detaching Punter");
+            punter = null;
+        }
+        else if (other.tag == "DrinkSource")
+        {
+            Debug.Log("Detaching DrinkSource.");
+            this.drinkSourceBehavour = null;
+        }
+    }
+}
---
+++ Assets/Scripts/PunterManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class PunterManager
+    {
+        public PunterManager(GameObject punterToClone, int maxPunters, float spawnPeriod)
+        {
+            this.numSpawnPoints = maxPunters;
+            this._punterToClone = punterToClone;
+            this._spawnPeriod = spawnPeriod;
+            this._currentPunters = new GameObject[this.numSpawnPoints];
+            this._spawnPoints = new Vector3[this.numSpawnPoints];
+            this._unOccupiedQueueIndexes = new List<int>();
+
+            for (int i = 0; i < this.numSpawnPoints; i++)
+            {
+                this._spawnPoints[i] = new Vector3(12f, 4f, 5f * (i - 3));
+                this._unOccupiedQueueIndexes.Add(i);
+            }
+        }
+
+        private GameObject _punterToClone;
+        private float _spawnPeriod;
+        private int punterID;
+        private int numSpawnPoints;
+
+        private Vector3[] _spawnPoints;
+        private GameObject[] _currentPunters;
+        private int _currentNumberOfPunters;
+
+        private List<int> _unOccupiedQueueIndexes;
+
+        private DateTime _nextSpawnTime = DateTime.Now;
+
+
+        public void Tick(float deltaTime)
+        {
+            DateTime Now = DateTime.Now;
+            if (this._nextSpawnTime < Now)
+            {
+                this.SpawnPunter();
+                this.UpdateNextSpawnTime(Now);
+            }
+            this.UpdatePunters();
+        }
+
+        private void SpawnPunter()
+        {
+            // select from the list of spawn points, and remove it from consideration next spawn
+            if (this._unOccupiedQueueIndexes.Count == 0)
+            {
+                Debug.Log("Cannot spawn a new punter as all queues are occupied.");
+                return;
+            }
+
+            int selectedQueueIndex = this._unOccupiedQueueIndexes[UnityEngine.Random.Range(0, this._unOccupiedQueueIndexes.Count)];
+            if (this._currentPunters[selectedQueueIndex] != null)
+            {
+                Debug.LogErrorFormat("Cannot use queue index {0} to spawn a punter. already one there!", selectedQueueIndex);
+                return;
+            }
+
+            GameObject punterToAdd = GameObject.Instantiate(this._punterToClone);
+            punterToAdd.GetComponent<PunterBehaviour>().Initialize();
+
+            this._unOccupiedQueueIndexes.Remove(selectedQueueIndex);
+            punterToAdd.transform.position = this._spawnPoints[selectedQueueIndex];
+
+            PunterController punterController = punterToAdd.GetComponent<PunterBehaviour>().punterController;
+            punterController.State = PunterState.ApproachingBar;
+            punterController.Id = this.punterID;
+            punterID++;
+            this._currentPunters[selectedQueueIndex] = punterToAdd;
+        }
+
+        private void UpdatePunters()
+        {
+            for (int i = 0; i < this._currentPunters.Length; i++)
+            {
+                if(this._currentPunters[i] == null)
+                {
+                    // AOF: this is horrifically inefficient, sorry everyone.
+                    if (!this._unOccupiedQueueIndexes.Contains(i))
+                    {
+                        this._unOccupiedQueueIndexes.Add(i);
+                    }
+                }
+                
+            }
+        }
+        
+        private void UpdateNextSpawnTime(DateTime now)
+        {
+            // TODO: add randomness
+            this._nextSpawnTime = now.AddSeconds(this._spawnPeriod);
+        }
+    }
+}
